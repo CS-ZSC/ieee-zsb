c22592a582bf72c63c8fabc6c8f1643cf5e8bf6b
hulxv <hulxxv@gmail.com>
2025-03-15 01:19:57 +0200

    chore: remove useless `global.css`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Header from "./components/Header";
 import Loading from "./loading";
 
 import { Provider } from "@/components/ui/provider"
 import { Box } from "@chakra-ui/react";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "IEEE ZSB",
@@ -29,13 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>
-
           <Header />
           {children}
-
         </Provider>
       </body>
     </html>
