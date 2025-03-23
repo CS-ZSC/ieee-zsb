6290a49114f9c63946aca0ec92099bd275b77ffc
hulxv <hulxxv@gmail.com>
2025-03-23 07:00:20 +0200

    feat: improve `Logo` component

--- components/ui/internal/logo.tsx
+++ components/ui/internal/logo.tsx
@@ -1,11 +1,24 @@
+'use client'
 import { Image } from "@chakra-ui/react";
 import Link from "next/link";
+import logoSvg from "@/assets/ieee-logo.svg"
+import { useColorMode } from "@/components/ui/color-mode";
+interface LogoOptions {
+  logoType: LogoType
+}
+export enum LogoType {
+  Blue = "blue",
+  White = "white"
+}
 
-export default function Logo() {
+export default function Logo({ logoType }: LogoOptions) {
+  const { colorMode } = useColorMode();
+  const __logoType = logoType ?? (colorMode === "light" ? LogoType.Blue : LogoType.White);
+
   return (
     <Link href="/" passHref>
       <Image
-        src="/Images/IEEE/IEEE Blue.svg"
+        src={`/Images/IEEE/ieee-logo-${__logoType}.svg`}
         alt="IEEE ZSB Logo"
         width={85}
         height={50}
