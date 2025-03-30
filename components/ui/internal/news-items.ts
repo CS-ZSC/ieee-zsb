5b675de94f9dd4bcae3c04ad50656261408acda2
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-03-30 15:31:09 +0200

    chore: adding fake news data

---
+++ components/ui/internal/news-items.ts
@@ -0,0 +1,34 @@
+const newsItems = [
+  {
+    image: "/Fake-Images/4.jpg",
+    title: "Palestine",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+  {
+    image: "/Fake-Images/5.jpg",
+    title: "Germany",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+  {
+    image: "/Fake-Images/6.jpg",
+    title: "England",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+  {
+    image: "/Fake-Images/7.jpg",
+    title: "Russia",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+  {
+    image: "/Fake-Images/8.jpg",
+    title: "China",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+];
+
+export default newsItems;
