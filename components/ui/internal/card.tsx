afb0631acef8cf83ffb1ccba66d252aef3c564d9
hulxv <hulxxv@gmail.com>
2025-04-12 02:46:49 +0200

    add global spacing for `Card` component

--- components/ui/internal/card.tsx
+++ components/ui/internal/card.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { Stack } from "@chakra-ui/react";
+import { Flex, Stack } from "@chakra-ui/react";
 
 export default function Card({ children }: { children: React.ReactNode }) {
     return (
-        <Stack
-            w="full"
-            align={"center"}
-            justify={"center"}
-            bgColor={"card-bg-3"}
-            color={"white"}
-            border="1px solid"
-            borderColor="card-border-3"
-            padding="var(--card-padding)"
-            borderRadius={20}
-            gapY={10}
-        >
-            {children}
-        </Stack>
+        <Flex p={"var(--global-spacing)"} w="full" h="full">
+            <Stack
+                w="full"
+                align={"center"}
+                justify={"center"}
+                bgColor={"card-bg-3"}
+                color={"white"}
+                border="1px solid"
+                borderColor="card-border-3"
+                padding="var(--card-padding)"
+                borderRadius={20}
+                gapY={10}
+                m={"var(--global-spacing)/2"}
+            >
+                {children}
+            </Stack>
+        </Flex>
     );
 }
