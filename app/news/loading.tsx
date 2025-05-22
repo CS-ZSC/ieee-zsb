c3ab8436904129b70770d12ebcc7ef76377e7b6d
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-05-22 15:12:39 +0300

    chore: update Loading component with ClipLoader animation

--- app/news/loading.tsx
+++ app/news/loading.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
+import { ClipLoader } from "react-spinners";
 
 export default function loading() {
   return (
-    <Flex
-      width={"full"}
-      height={"100vh"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      fontSize={"3xl"}
-    >
-      Loading News
+    <Flex justifyContent={"center"} alignItems={"center"} gap={5}>
+      <Flex
+        height={"100vh"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        fontSize={"3xl"}
+      >
+        Loading News
+      </Flex>
+
+      <ClipLoader color="var(--main-identity-color)" size={40} />
     </Flex>
   );
 }
