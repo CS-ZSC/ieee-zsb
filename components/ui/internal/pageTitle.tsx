6fdbc5813cb4a87ad58f2e1f52894132234a5393
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-06-20 02:06:45 +0300

    feat: implement Animation Card for better visualizaion when scrolling

--- components/ui/internal/pageTitle.tsx
+++ components/ui/internal/pageTitle.tsx
@@ -1,14 +1,15 @@
+"use client";
+
 import React from "react";
 import { Text } from "@chakra-ui/react";
+import AnimatedCard from "./animatedCard";
 
 export default function PageTitle({ title }: { title: string }) {
   return (
-    <Text
-      fontWeight={"bold"}
-      fontSize={"3rem"}
-      textAlign={"center"}
-    >
-      {title}
-    </Text>
+    <AnimatedCard>
+      <Text fontWeight={"bold"} fontSize={"3rem"} textAlign={"center"}>
+        {title}
+      </Text>{" "}
+    </AnimatedCard>
   );
 }
