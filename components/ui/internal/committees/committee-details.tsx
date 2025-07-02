0c8b34470381c5016c082e1ac6626494583c3889
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-07-02 16:17:47 +0300

    feat: Committees components

---
+++ components/ui/internal/committees/committee-details.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Flex, Text } from "@chakra-ui/react";
+import type { CommitteesData } from "@/data/committees";
+import { AnimatePresence, motion } from "framer-motion";
+import Card from "@/components/ui/internal/card";
+import { useWindowType } from "@/hooks/use-window-type";
+import LeadersContainer from "../leaders-container";
+
+const MotionDiv = motion.div;
+interface CommitteeDetailsProps {
+  committee: CommitteesData;
+  isOpen: boolean;
+  borderColor: string;
+  positionBgColor: string;
+}
+
+export default function CommitteeDetails({
+  committee,
+  isOpen,
+  borderColor,
+  positionBgColor,
+}: CommitteeDetailsProps) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const { isDesktop } = useWindowType();
+
+  return (
+    <AnimatePresence initial={false}>
+      {isOpen && (
+        <Card bgColor="primary-12" padding={"var(--global-spacing)"}>
+          <MotionDiv
+            initial={{ height: 0 }}
+            animate={{ height: contentRef.current?.scrollHeight || "auto" }}
+            exit={{ height: 0 }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+            style={{ overflow: "hidden", alignSelf: "start" }}
+          >
+            <Flex flexDirection={"column"} gap={5}>
+              <Flex flexDirection={"column"}>
+                <SectionTitle text="Goals" />
+                <Flex>
+                  <Card bgColor="primary-12" rounded="xl" gap={6}>
+                    {committee.goals.map((goal, index) => (
+                      <Text
+                        key={index}
+                        color={"neutral-2"}
+                        borderLeft={index == 0 ? "2px solid" : ""}
+                        borderColor={borderColor}
+                        paddingLeft={"10px"}
+                        alignSelf={"flex-start"}
+                        width={"full"}
+                      >
+                        {goal}
+                      </Text>
+                    ))}
+                  </Card>
+                </Flex>
+              </Flex>
+              <Flex flexDirection={"column"}>
+                <SectionTitle text="Activities" />
+                <Flex
+                  gap={"var(--global-spacing)"}
+                  flexDirection={isDesktop ? "row" : ""}
+                  flexWrap={"wrap"}
+                  justifyContent={"center"}
+                >
+                  {committee.activities?.map((activity, index) => (
+                    <Flex
+                      key={index}
+                      width={isDesktop ? `calc(100% / 3 - 14px)` : "300px"}
+                    >
+                      <Card bgColor="primary-12" gap={2}>
+                        <Text
+                          color={"primary-8"}
+                          fontSize={"2xl"}
+                          fontWeight={"bold"}
+                        >
+                          {activity.title}
+                        </Text>
+                        <Text color={"neutral-2"} height={"full"}>
+                          {activity.content}
+                        </Text>
+                      </Card>
+                    </Flex>
+                  ))}
+                </Flex>
+              </Flex>
+              <Flex flexDirection={"column"}>
+                <SectionTitle text="Leaders" />
+                <LeadersContainer
+                  positions={committee.board}
+                  positionBgColor={positionBgColor}
+                />
+              </Flex>
+            </Flex>
+          </MotionDiv>
+        </Card>
+      )}
+    </AnimatePresence>
+  );
+}
+
+function SectionTitle({ text }: { text: string }) {
+  const { isDesktop } = useWindowType();
+
+  return (
+    <Text
+      color="neutral-1"
+      padding="10px 25px"
+      fontWeight="bold"
+      fontSize="2xl"
+      textAlign={isDesktop ? "left" : "center"}
+    >
+      {text}
+    </Text>
+  );
+}
---
+++ components/ui/internal/committees/committee.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useState } from "react";
+import { Flex, Heading } from "@chakra-ui/react";
+import type { CommitteesData } from "@/data/committees";
+import CommitteeBrief from "./committee-brief";
+import CommitteeDetails from "./committee-details";
+import AnimatedCard from "@/components/ui/internal/animatedCard";
+import { useWindowType } from "@/hooks/use-window-type";
+
+interface CommitteeProps {
+  committee: CommitteesData;
+  borderColor: string;
+  positionBgColor: string;
+}
+
+export default function Committee({
+  committee,
+  borderColor,
+  positionBgColor,
+}: CommitteeProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { isDesktop } = useWindowType();
+
+  return (
+    <AnimatedCard>
+      <Flex
+        id={committee.hashtag}
+        direction="column"
+        gap={"var(--global-spacing)"}
+      >
+        <Heading
+          fontSize="3xl"
+          fontWeight="bold"
+          paddingLeft={isDesktop ? "20px" : "0px"}
+          textAlign={isDesktop ? "left" : "center"}
+        >
+          {committee.name}
+        </Heading>
+        <CommitteeBrief
+          description={committee.description}
+          image={committee.image}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+        <CommitteeDetails
+          committee={committee}
+          isOpen={isOpen}
+          borderColor={borderColor}
+          positionBgColor={positionBgColor}
+        />
+      </Flex>
+    </AnimatedCard>
+  );
+}
