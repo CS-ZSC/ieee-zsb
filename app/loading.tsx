c5f5e9b57922fa558865f93e3932027517a1ddee
Mohamed Emad <hulxxv@gmail.com>
2025-08-01 11:16:45 +0300

    improve loading (#43)
    Closes #34

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,21 +1,21 @@
 "use client";
-import { PacmanLoader } from "react-spinners";
-import { Flex } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 import { useColorModeValue } from "@/components/ui/color-mode";
+import { MoonLoader } from "react-spinners";
 
 export default function Loading() {
   const indicatorColor = useColorModeValue("black", "white");
+  const subTextColor = useColorModeValue("gray.600", "gray.400");
 
   return (
-    <Flex
-      position="absolute"
-      top="50%"
-      left="50%"
-      transform="translate(-50%, -50%)"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <PacmanLoader color={indicatorColor} size={50} />
-    </Flex>
+    <Stack height="100vh" width="100vw" justify="center" align="center" spacing={6} bg={"primary-1"}>
+      <MoonLoader color={indicatorColor} size={50} speedMultiplier={1.2} />
+      <Text fontSize="2xl" fontWeight="bold" color={indicatorColor} marginTop={2} textAlign="center">
+        IEEE is charging the electrons...
+      </Text>
+      <Text fontSize="md" color={subTextColor} maxW="sm" textAlign="center">
+        Please wait while we align the bits and bytes. Your data is being processed at the speed of light (almost)!
+      </Text>
+    </Stack>
   );
 }
