54736a40a7a195997237b4f154398ec4bf4a486a
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-08-01 15:35:56 +0300

    fix: remove spacing property as it doesn't exist in Stack

--- app/loading.tsx
+++ app/loading.tsx
@@ -8,7 +8,7 @@ export default function Loading() {
   const subTextColor = useColorModeValue("gray.600", "gray.400");
 
   return (
-    <Stack height="100vh" width="100vw" justify="center" align="center" spacing={6} bg={"primary-1"}>
+    <Stack height="100vh" width="100vw" justify="center" align="center" bg={"primary-1"}>
       <MoonLoader color={indicatorColor} size={50} speedMultiplier={1.2} />
       <Text fontSize="2xl" fontWeight="bold" color={indicatorColor} marginTop={2} textAlign="center">
         IEEE is charging the electrons...
