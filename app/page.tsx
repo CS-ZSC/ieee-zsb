482073f00743c24c2736c2f55d6203dbecfb30a3
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-08-01 15:15:29 +0300

    fix: remove old effects in hero section of home

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 
 import PageWrapper from "@/components/ui/internal/page-wrapper";
 import { Flex } from "@chakra-ui/react";
-import HeroSection from "@/components/ui/internal/home/hero-section";
 import Events from "@/components/ui/internal/home/events";
 import Brief from "@/components/ui/internal/home/brief";
 import News from "@/components/ui/internal/home/news";
+import { HeroCard } from "@/components/ui/internal/chapters/hero-card";
 
 export default function Home() {
   return (
@@ -15,7 +15,10 @@ export default function Home() {
         paddingBottom={"0px"}
         gap={"var(--global-spacing)"}
       >
-        <HeroSection />
+        <HeroCard
+          colorScheme="primary-1"
+          logo="/Images/IEEE/ieee-logo-white.svg"
+        />
         <Brief />
         <News />
         <Events />
