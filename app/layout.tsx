5ae6e1f4a277f1eee9f97a0addaf6e460d1039a8
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-08-05 14:04:40 +0300

    fix: add html attribute for smooth scrolling due to console warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <body data-new-gr-c-s-check-loaded="8.932.0" data-gr-ext-installed="">
         <Provider>
           <Stack
